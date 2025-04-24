1f4061c2b243b960b52439d0f77817294abda0ba
Jorge Albarracin <albarracinj.dev@gmail.com>
2025-04-24 09:17:04 -0300

    UPDATE: let cronjob available

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,18 @@ import cron from 'node-cron';
 const app = express();
 const port = 3000;
 
-// cron.schedule('10 11,13,15 * * 1-5', async () => {
+cron.schedule('*/10 11-15 * * 1-5', async () => {
 // cron.schedule('* * * * *', async () => {
-//     await main(0);
-// })
-
-app.get('/', async (req, res) => {
     await main(0);
-    res.json({result: "ok"});
-});
+})
+
+// await main(0);
+
+// app.get('/', async (req, res) => {
+//     await main(0);
+//     res.json({result: "ok"});
+// });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running`);
 });
