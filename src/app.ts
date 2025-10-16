d385c59f817c6609d6aff22661545902bdc1790c
albarracin-jorge <albarracinj.dev@gmail.com>
2025-10-16 18:32:31 -0300

    [add] main path return conection status

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { main } from './scrapper.js';
 import cron from 'node-cron'; 
+import { drizzle } from "drizzle-orm/mysql2";
+import { quotesTable } from './db/schema.js';
 
 const app = express();
 const port = 3000;
@@ -12,7 +14,11 @@ cron.schedule('*/3 11-15 * * 1-5', async () => {
 // await main(5);
 
 app.get('/', async (req, res) => {
-    res.json({result: "server running"});
+    const db = drizzle(process.env.DATABASE_URL!);
+    const [result] = await db.select().from(quotesTable).execute();
+    console.log("✅ Conexión exitosa:", result);
+    if(!result) return res.status(500).json({error: "Database connection error"});
+    res.json({result: "Server running"});
 });
 
 app.listen(port, () => {
